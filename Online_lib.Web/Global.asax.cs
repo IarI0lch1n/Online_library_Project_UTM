7005d1fe3b042643229d681707f0bb1dd03d64c0
1lex0_Griceanii <152962153+1lex0@users.noreply.github.com>
2025-05-07 11:16:33 +0300

    Updateing web part

---
+++ Online_lib.Web/Global.asax.cs
@@ -0,0 +1,28 @@
+﻿using Online_lib.BusinessLogic.DBModel;
+using Online_lib.Web.App_Start;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace Online_lib.Web
+{
+    public class Global : HttpApplication
+    {
+        void Application_Start(object sender, EventArgs e)
+        {
+            // Code that runs on application startup
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            Database.SetInitializer(new CreateDatabaseIfNotExists<UserContext>());
+        }
+    }
+}
