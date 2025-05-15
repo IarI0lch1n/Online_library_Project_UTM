da03fbe354112317f19cf57a64292fc1d3ab977c
1lex0_Griceanii <152962153+1lex0@users.noreply.github.com>
2025-05-15 21:45:24 +0300

    Book models

---
+++ Online_lib.Domain/Entities/User/BookViewModel.cs
@@ -0,0 +1,22 @@
+﻿namespace Online_lib.Domain.Entities.User
+{
+    public class BookViewModel
+    {
+        public int Id { get; set; }
+        public string BookName { get; set; }
+        public string Description { get; set; }
+        public string Edition { get; set; }
+        public decimal Price { get; set; }
+        public int Pages { get; set; }
+        public string Language { get; set; }
+        public string Genres { get; set; }
+        public int PublisherId { get; set; }
+        public int AuthorId { get; set; }
+        public DateTime? PublisherDate { get; set; }
+        public int ActualStock { get; set; }
+        public int CurrentStock { get; set; }
+        public int IssuedBooks { get; set; }
+
+        // файл лучше оставить в Web модели
+    }
+}
---
+++ Online_lib.Domain/Entities/User/UserBook.cs
@@ -0,0 +1,26 @@
+﻿namespace Online_lib.Domain.Entities.User
+{
+    public class UserBook
+    {
+        public int Id { get; set; }  // ID книги (обязателен для EF)
+        public string Title { get; set; }  // Название книги
+        public string Author { get; set; }  // Автор книги
+        public string Status { get; set; }  // Статус (Available, Issued и т.д.)
+        public int? UserId { get; set; }  // Владелец (пользователь)
+        public string BookName { get; set; }
+        public string Description { get; set; }
+        public string Edition { get; set; }
+        public decimal Price { get; set; }
+        public int Pages { get; set; }
+        public string Language { get; set; }
+        public string Genres { get; set; }
+        public int PublisherId { get; set; }
+        public int AuthorId { get; set; }
+        public DateTime? PublisherDate { get; set; }
+        public int ActualStock { get; set; }
+        public int CurrentStock { get; set; }
+        public int IssuedBooks { get; set; }
+        public string FilePath { get; set; } // путь к pdf-файлу
+        public string FileName { get; set; } // оригинальное имя файла
+    }
+}
