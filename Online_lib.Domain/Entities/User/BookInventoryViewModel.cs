8e86a95109d7ed6b970e5ef064d82033182db53d
IarI0lch1n <sniperazra1@gmail.com>
2025-05-19 12:01:09 +0300

    adding universal book model

---
+++ Online_lib.Domain/Entities/User/BookInventoryViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Online_lib.Domain.Entities.User
+{
+    public class BookInventoryViewModel
+    {
+        public BookViewModel FormData { get; set; } = new BookViewModel(); // ✅ важно!
+        public List<UserBook> Books { get; set; } = new List<UserBook>();
+    }
+}
+    
