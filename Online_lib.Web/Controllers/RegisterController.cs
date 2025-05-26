8a84e3a54e1e41afe610a6050f7d67008e4592a0
1lex0_Griceanii <152962153+1lex0@users.noreply.github.com>
2025-05-26 16:03:23 +0300

    retargeting to BL and updating some methods

--- Online_lib.Web/Controllers/RegisterController.cs
+++ Online_lib.Web/Controllers/RegisterController.cs
@@ -1,49 +1,42 @@
-﻿using Online_lib.BusinessLogic.DBModel;
+using Online_lib.BusinessLogic;
+using Online_lib.BusinessLogic.DBModel;
 using Online_lib.Domain.Entities.User;
-using System.Linq;
 using System.Web.Mvc;
-using System.Web.Security;
 
 namespace Online_lib.Web.Controllers
 {
     public class RegisterController : Controller
     {
-        private UserContext db = new UserContext();
+        private readonly SessionBL _sessionBL;
 
-        // GET: Register
+        public RegisterController()
+        {
+            _sessionBL = new SessionBL(new UserContext());
+        }
+
+        public ActionResult Index()
+        {
+            return View("_UserSignIn");
+        }
+
         public ActionResult Register()
         {
             return View();
         }
 
-        // POST: Register
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Register(UserProfileModel model)
         {
-            //if (ModelState.IsValid)
+            var result = _sessionBL.RegisterUser(model);
+
+            if (result.Status)
             {
-                var newUser = new UDbTable
-                {
-                    Username = model.FullName,
-                    Email = model.Email,
-                    Password = model.NewPassword,
-                    City = model.City,
-                    State = model.State,
-                    Pincode = model.Pincode ?? 0,
-                    DateOfBirth = model.DateOfBirth,
-                    ContactNumber = model.ContactNumber,
-                    //FullAddress = model.FullAddress ?? "N/A",
-                    // и т.д.
-                };
-
-                db.Users.Add(newUser);   // <== создаст запись
-                db.SaveChanges();        // <== создаст таблицу, если её нет
-
-                ViewBag.Message = "Пользователь успешно зарегистрирован!";
-                return RedirectToAction("Login", "Login");
+                ViewBag.Message = result.StatusMsg;
+                return RedirectToAction("Index", "Home");
             }
 
+            ViewBag.Error = result.StatusMsg;
             return RedirectToAction("Success");
         }
 
@@ -51,7 +44,5 @@ namespace Online_lib.Web.Controllers
         {
             return View();
         }
-
-        
     }
 }
