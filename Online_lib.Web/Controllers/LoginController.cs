40b42843fcd3d7369ad7b444bf6a8eb477c7566f
1lex0_Griceanii <152962153+1lex0@users.noreply.github.com>
2025-05-26 15:59:27 +0300

    retargeting to BL and updating some methods

--- Online_lib.Web/Controllers/LoginController.cs
+++ Online_lib.Web/Controllers/LoginController.cs
@@ -1,80 +1,55 @@
 using System;
-using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Security;
 using Online_lib.Domain.Entities.User;
+using Online_lib.BusinessLogic;
 using Online_lib.BusinessLogic.DBModel;
-using System.Security.Cryptography;
-using System.Text;
-
 
 namespace Online_lib.Web.Controllers
 {
     public class LoginController : Controller
     {
-        private UserContext db = new UserContext();
+        private readonly SessionBL _sessionBL;
+
+        public LoginController()
+        {
+            _sessionBL = new SessionBL(new UserContext());
+        }
 
         public ActionResult Index()
         {
             return View("~/Views/Shared/_UserLogin.cshtml", new ULoginData());
         }
-
         [HttpPost]
         public ActionResult Login(ULoginData login)
         {
-            var userLogin = CheckLoginData(login);
+            var result = _sessionBL.UserLogin(login);
 
-            if (userLogin.Status)
+            if (result.Status && result.User != null)
             {
                 FormsAuthentication.SetAuthCookie(login.Name, false);
 
-                var user = db.Users.FirstOrDefault(u =>
-                    (u.Email == login.Name || u.ContactNumber == login.Name) &&
-                    u.Password == login.Password);
+                var user = result.User;
 
-                if (user != null)
+                var cookie = new HttpCookie("LoginToken", result.HmacToken)
                 {
-                    string guidToken = Guid.NewGuid().ToString();
-                    string secretKey = "SuperStrongKey123!"; // желательно вынести в web.config
+                    Expires = DateTime.Now.AddDays(7),
+                    HttpOnly = true
+                };
+                Response.Cookies.Add(cookie);
 
-                    string hmacToken = GenerateHmac(guidToken, secretKey);
-
-                    var cookie = new HttpCookie("LoginToken", hmacToken)
-                    {
-                        Expires = DateTime.Now.AddDays(7),
-                        HttpOnly = true
-                    };
-                    Response.Cookies.Add(cookie);
-
-                    user.LoginToken = guidToken;
-                    db.SaveChanges();
-
-                    Session["UserID"] = user.Id;
-                    Session["UserFullName"] = user.Username;
-                    Session["UserRole"] = user.Role;
-                }
+                Session["UserID"] = user.Id;
+                Session["UserFullName"] = user.Username;
+                Session["UserRole"] = user.Role;
 
                 return RedirectToAction("Dashboard", "Profile");
             }
 
-            ViewBag.Error = "Неверный логин или пароль";
+            ViewBag.Error = result.StatusMsg;
             return View("~/Views/Shared/_UserLogin.cshtml", login);
         }
 
-        public ULoginData CheckLoginData(ULoginData data)
-        {
-            var user = db.Users.FirstOrDefault(u =>
-                (u.Email == data.Name || u.ContactNumber == data.Name) &&
-                u.Password == data.Password);
-
-            return new ULoginData
-            {
-                Name = data.Name,
-                Password = data.Password,
-                Status = user != null
-            };
-        }
 
         public ActionResult Logout()
         {
@@ -92,17 +67,6 @@ namespace Online_lib.Web.Controllers
             }
 
             return RedirectToAction("Index", "Home");
-        }
-        private string GenerateHmac(string input, string key)
-        {
-            var keyBytes = Encoding.UTF8.GetBytes(key);
-            var inputBytes = Encoding.UTF8.GetBytes(input);
-
-            using (var hmac = new HMACSHA256(keyBytes))
-            {
-                var hashBytes = hmac.ComputeHash(inputBytes);
-                return Convert.ToBase64String(hashBytes);
-            }
         }
     }
 }
