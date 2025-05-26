d9d3c9cd30d44a26cb1eda3505fdc3e52a9b4cf3
1lex0_Griceanii <152962153+1lex0@users.noreply.github.com>
2025-05-26 15:42:43 +0300

    Adding author's and publisher's models

---
+++ Online_lib.Domain/Entities/User/Author.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Online_lib.Domain.Entities.User
+{
+    public class Author
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Country { get; set; }
+        public DateTime? DateOfDeath { get; set; }
+    }
+}
---
+++ Online_lib.Domain/Entities/User/Publisher.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Online_lib.Domain.Entities.User
+{
+    public class Publisher
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public DateTime RegistrationDate { get; set; }
+    }
+}
