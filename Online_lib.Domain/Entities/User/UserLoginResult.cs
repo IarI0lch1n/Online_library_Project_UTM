1d29d25d5126cc19047bc1135e02b5f09606898a
1lex0_Griceanii <152962153+1lex0@users.noreply.github.com>
2025-05-26 15:48:09 +0300

    Adding Hmac token to user

--- Online_lib.Domain/Entities/User/UserLoginResult.cs
+++ Online_lib.Domain/Entities/User/UserLoginResult.cs
@@ -1,8 +1,11 @@
-﻿namespace Online_lib.Domain.Entities.User
+namespace Online_lib.Domain.Entities.User
 {
     public class UserLoginResult
     {
         public bool Status { get; set; }
         public string StatusMsg { get; set; }
+        public UDbTable User { get; set; }         
+        public string HmacToken { get; set; }     
+
     }
 }
