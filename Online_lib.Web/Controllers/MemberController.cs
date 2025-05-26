34afb384cf531ca0d3b8a6949fb4fb49ab81a1da
1lex0_Griceanii <152962153+1lex0@users.noreply.github.com>
2025-05-26 16:00:31 +0300

    retargeting to BL and updating some methods

--- Online_lib.Web/Controllers/MemberController.cs
+++ Online_lib.Web/Controllers/MemberController.cs
@@ -1,21 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System;
 using System.Web.Mvc;
-using Online_lib.Domain.Entities.User;
+using Online_lib.BusinessLogic;
 using Online_lib.BusinessLogic.DBModel;
-using Online_lib.Domain.Enums;
 
 namespace Online_lib.Web.Controllers
 {
     public class MemberController : Controller
     {
-        private UserContext db = new UserContext();
+        private readonly MemberBL _memberBL;
+
+        public MemberController()
+        {
+            _memberBL = new MemberBL(new UserContext());
+        }
 
         public ActionResult ManageUsers(int? selectedId = null)
         {
-            var allUsers = db.Users.ToList();
-            var selectedUser = selectedId.HasValue ? db.Users.Find(selectedId.Value) : null;
+            var allUsers = _memberBL.GetAllUsers();
+            var selectedUser = selectedId.HasValue ? _memberBL.GetUserById(selectedId.Value) : null;
 
             var model = Tuple.Create(allUsers, selectedUser);
             Session["HomepageView"] = null;
@@ -25,37 +27,20 @@ namespace Online_lib.Web.Controllers
 
         public ActionResult ChangeRole(int id)
         {
-            var user = db.Users.Find(id);
-            if (user != null)
-            {
-                user.Role = user.Role == UserRole.Admin ? UserRole.User : UserRole.Admin;
-                db.SaveChanges();
-            }
+            _memberBL.ChangeRole(id);
             return RedirectToAction("ManageUsers", new { selectedId = id });
         }
 
         public ActionResult ToggleBlock(int id)
         {
-            var user = db.Users.Find(id);
-            if (user != null)
-            {
-                user.IsBlocked = !user.IsBlocked;
-                db.SaveChanges();
-            }
+            _memberBL.ToggleBlock(id);
             return RedirectToAction("ManageUsers", new { selectedId = id });
         }
 
         public ActionResult DeleteUser(int id)
         {
-            var user = db.Users.Find(id);
-            if (user != null)
-            {
-                db.Users.Remove(user);
-                db.SaveChanges();
-            }
-
+            _memberBL.DeleteUser(id);
             return RedirectToAction("ManageUsers");
         }
-
     }
 }
